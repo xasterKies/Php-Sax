50f03ae5696b0b50161d5b56a4d2dccb5d7043b7
TiokengSamuel <samueledward40@gmail.com>
2021-04-06 13:48:46 +0100

    Starting shout box php project

---
+++ Shout it out/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+    <title>Shout it</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Shout It Box</h1>
+        </header>
+        <div id="shout">
+            <ul>
+                <li class="shout"><span> 10:15Pm -</li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
