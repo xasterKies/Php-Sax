c4da644db7e2a833a95a638c1dcb01d260c5b5f1
TiokengSamuel <samueledward40@gmail.com>
2021-04-06 14:02:04 +0100

    markup ui complete

--- Shout it out/index.php
+++ Shout it out/index.php
@@ -8,15 +8,28 @@
     <title>Shout it</title>
 </head>
 <body>
-    <div class="container">
+    <div id="container">
         <header>
             <h1>Shout It Box</h1>
         </header>
-        <div id="shout">
+        <div id="shouts">
             <ul>
-                <li class="shout"><span> 10:15Pm -</li>
+                <li class="shout"><span> 10:15Pm -</span> Samuel: Hey what are you guys up to?</li>
+                <li class="shout"><span> 10:15Pm -</span> Samuel: Hey what are you guys up to?</li>
+                <li class="shout"><span> 10:15Pm -</span> Samuel: Hey what are you guys up to?</li>
+                <li class="shout"><span> 10:15Pm -</span> Samuel: Hey what are you guys up to?</li>
+                <li class="shout"><span> 10:15Pm -</span> Samuel: Hey what are you guys up to?</li>
             </ul>
         </div>
+        <div id="input">
+            <form action="process.php" method="post">
+                <input type="text" name="user" placeholder="Enter Your Name" />
+                <input type="text" name="message" placeholder="Enter a Message" />
+                <br>
+                <input class="shout" type="submit" name="submit" value="Shout it"/>
+            </form>
+        </div>
     </div>
+
 </body>
 </html>
