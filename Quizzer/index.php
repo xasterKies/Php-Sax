4411724eba8490ec0086837fd7f0bdc775437276
TiokengSamuel <samueledward40@gmail.com>
2021-04-24 22:23:10 +0100

    PHP markup

--- Quizzer/index.php
+++ Quizzer/index.php
@@ -16,6 +16,14 @@
     <main>
         <div class="container">
             <h2>Test Your PHP Knowledge</h2>
+            <p>This is a multiple choice quiz to test your knowledge of P</p>
+            <ul>
+                <li><strong>Number of Questions: </strong>5</li>
+                <li><strong>Type: </strong>Multiple Choice</li>
+                <li><strong>Estimated Time: </strong>4 Minutes</li>
+
+            </ul>
+            <a href="question.php?n=1" class="start">Start Quiz</a>
         </div>
     </main>
     <footer>
