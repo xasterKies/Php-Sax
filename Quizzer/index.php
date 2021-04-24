f101225b759a3f16b7f3cfec91ec759b57c430ed
TiokengSamuel <samueledward40@gmail.com>
2021-04-24 21:39:26 +0100

    Setting up Quizzer

---
+++ Quizzer/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quizzer</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+</head>
+<body>
+    <header>
+        <div class="container">
+            <h1>PHP Quizzer</h1>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <h2>Test Your PHP Knowledge</h2>
+        </div>
+    </main>
+    <footer>
+        <div class="container">
+             Copywright &copy; 2021, PHP Quizzer       
+         </div>
+    </footer>
+</body>
+</html>
