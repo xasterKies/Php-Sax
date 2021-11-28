71e8cd7dcaea5a574a1235df41acc0b3814a4517
xaster-Kies <samueledward40@gmail.com>
2021-11-29 00:39:20 +0100

    initialising name with get

---
+++ PHP-Beginning/Count-Template/count.php
@@ -0,0 +1,10 @@
+<?php 
+    
+    $output = '';
+
+    for($count; $count <= 20; $count++) {
+        $output .= $count . ' ';
+    }
+
+    include './count.html.php';
+?>
---
+++ PHP-Beginning/Name/name.php
@@ -0,0 +1,6 @@
+<?php 
+
+    $name = $_GET['name'];
+    echo 'Welcome to our website ' . $name . '!';
+
+?>
