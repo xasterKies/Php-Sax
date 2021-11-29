64a71c2e40b626a46a653bdca8794b85511ab01d
xaster-Kies <samueledward40@gmail.com>
2021-11-29 07:46:28 +0100

    did a simple login

--- PHP-Beginning/Name/name.php
+++ PHP-Beginning/Name/name.php
@@ -1,6 +1,7 @@
 <?php 
 
-    $name = $_GET['name'];
-    echo 'Welcome to our website ' . $name . '!';
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    echo 'Welcome to our website ' . htmlspecialchars($firstname, ENT_QUOTES, 'UTF-8') . ' ' . htmlspecialchars($lastname, ENT_QUOTES, 'UTF-8') . '!';
 
 ?>
